45a2c79526f7610e57233106c10abaa352313d62
Carlos Furnari <carlosfurnari@gmail.com>
2025-08-15 22:00:00 -0300

    Change Sheets func

--- netlify/functions/rsvp.js
+++ netlify/functions/rsvp.js
@@ -3,7 +3,7 @@ exports.handler = async function(event, context) {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
   const payload = JSON.parse(event.body);
-  const response = await fetch('https://script.google.com/macros/s/AKfycbx_4nzgJMTK-PMOhT7lch69ZvrGv4HVPNAbkBmfX9KDoWiFXRvo6MozIfZ8vzNlDx8xaQ/exec', {
+  const response = await fetch('https://script.google.com/macros/s/AKfycbwQXq7dFbtEgTGoYtRQD8NEHxSQFnyD0JYKcnWjWqpnb5ePU2ISM-2Af53EnbEQpKYi/exec', {
     method: 'POST',
     body: JSON.stringify(payload),
     headers: { 'Content-Type': 'application/json' }
